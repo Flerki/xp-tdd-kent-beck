fa4cbf666322f45ac44af30a65f3686332ea4a0c
Andrei <mairovichaa@yandex.ru>
2021-04-03 18:07:59 +0300

    Complete Chapter 4

--- part_1/src/test/java/com/amairovi/DollarTest.java
+++ part_1/src/test/java/com/amairovi/DollarTest.java
@@ -9,14 +9,12 @@ class DollarTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
-    void testEquality(){
+    void testEquality() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(6));
     }
