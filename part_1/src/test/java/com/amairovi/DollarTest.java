b38a5d2be4b8f45f156b21e041dbde76e8325ad8
Andrei <mairovichaa@yandex.ru>
2021-04-03 18:11:33 +0300

    Complete Chapter 5

--- part_1/src/test/java/com/amairovi/DollarTest.java
+++ part_1/src/test/java/com/amairovi/DollarTest.java
@@ -12,6 +12,12 @@ class DollarTest {
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
+    @Test
+    void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
 
     @Test
     void testEquality() {
