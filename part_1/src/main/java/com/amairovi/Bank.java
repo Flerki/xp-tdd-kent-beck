b9c4fa5d52a6aef6d9300fb6ad71b7a051fb582d
Andrei <mairovichaa@yandex.ru>
2021-04-04 13:59:22 +0300

    Complete Chapter 14

--- part_1/src/main/java/com/amairovi/Bank.java
+++ part_1/src/main/java/com/amairovi/Bank.java
@@ -1,7 +1,47 @@
 package com.amairovi;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Bank {
+    private Map<Pair, Integer> rates = new HashMap<>();
+
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
+    }
+
+    public int rate(String from, String to) {
+        if (from.equals(to)) {
+            return 1;
+        }
+        return rates.get(new Pair(from, to));
+    }
+
+    private static class Pair {
+        private final String from;
+        private final String to;
+
+        Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return Objects.equals(from, pair.from) && Objects.equals(to, pair.to);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(from, to);
+        }
     }
 }
