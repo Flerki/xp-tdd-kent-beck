c8671efbd4249830ed8dab3d5b070cfaf649706d
Andrei <mairovichaa@yandex.ru>
2021-04-04 13:42:22 +0300

    Complete Chapter 13

--- part_1/src/main/java/com/amairovi/Expression.java
+++ part_1/src/main/java/com/amairovi/Expression.java
@@ -1,4 +1,5 @@
 package com.amairovi;
 
 public interface Expression {
+    Money reduce(String to);
 }
--- part_1/src/test/java/com/amairovi/DollarTest.java
+++ part_1/src/test/java/com/amairovi/DollarTest.java
@@ -29,10 +29,26 @@ class DollarTest {
     @Test
     void testSimpleAddition() {
         Money five = Money.dollar(5);
-        Expression sum = five.plus(five);
+        Expression result = five.plus(five);
+        Sum sum = (Sum) result;
         Bank bank = new Bank();
         Money reduced = bank.reduce(sum, "USD");
         assertEquals(Money.dollar(10), reduced);
+    }
+
+    @Test
+    void testReduceSum() {
+        Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+        Bank bank = new Bank();
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(7), result);
+    }
+
+    @Test
+    void testReduceMoney() {
+        Bank bank = new Bank();
+        Money result = bank.reduce(Money.dollar(1), "USD");
+        assertEquals(Money.dollar(1), result);
     }
 
 }
